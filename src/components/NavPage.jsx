d646b02275d38b3c6e0f95da162ec690282c3432
simon0820s <simon0820s@gmail.com>
2023-05-21 16:23:09 -0500

    finish app

--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -1,15 +1,54 @@
 import React from 'react'
+import {HiArrowLongRight, HiArrowLongLeft} from "react-icons/hi2";
 
 const NavPage = (props) => {
-  return (
-    <header className='d-flex justify-content-between align-items-center'>
-        <p>Page : {props.page}</p>
-        <button className='btn btn-primary btn-sm'
-        onClick={()=>props.setPage(props.page+1)}>
-            Page: {props.page+1} 
-        </button>
-    </header>
-  )
+
+    if (props.page==1){
+        return (
+            <header className='d-flex justify-content-between align-items-center'>
+                <p>Page : {props.page}</p>
+        
+                    <button className='btn btn-primary btn-sm mx-1'
+                    onClick={()=>props.setPage(props.page+1)}>
+                         <HiArrowLongRight className='display-6'/>   
+                    </button>
+                
+            </header>
+          )
+
+    }else if(props.page==42){
+        return (
+            <header className='d-flex justify-content-between align-items-center'>
+                <p>Page : {props.page}</p>
+        
+                    <button className='btn btn-primary btn-sm mx-1'
+                    onClick={()=>props.setPage(props.page-1)}>
+                        <HiArrowLongLeft className='display-6'/>
+                    </button>
+                
+            </header>
+          )
+    }else{
+        return (
+            <header className='d-flex justify-content-between align-items-center'>
+                <p>Page : {props.page}</p>
+        
+                <div>
+                    <button className='btn btn-primary btn-sm mx-1'
+                    onClick={()=>props.setPage(props.page-1)}>
+                        <HiArrowLongLeft className='display-6'/>
+                    </button>
+        
+                    <button className='btn btn-primary btn-sm mx-1'
+                    onClick={()=>props.setPage(props.page+1)}>
+                         <HiArrowLongRight className='display-6'/>   
+                    </button>
+                </div>
+                
+            </header>
+          )
+    }
+  
 }
 
 export default NavPage
